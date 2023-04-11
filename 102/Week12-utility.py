c850ba33e7b13d64cf1ed7127b277b70d8dcf614
NaveenMag <129795361+NaveenMag@users.noreply.github.com>
2023-04-11 14:18:44 -0600

    Added fun 2
    Created the update string

--- 102/Week12-utility.py
+++ 102/Week12-utility.py
@@ -1,6 +1,7 @@
+
 def load_file(filename):
     with open(filename, 'r') as file:
         readfile = file.read().splitlines()
         return readfile
-lines = load_file('mother.txt')
-print(lines)
+def update_string(string1,string2,integer):
+    return f'{string1[0:integer]}{string2}{string1[integer + 1:]}'
