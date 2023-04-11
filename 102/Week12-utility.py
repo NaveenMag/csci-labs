cefe077cee3abd5c37aef1c477dc152419a4f722
NaveenMag <129795361+NaveenMag@users.noreply.github.com>
2023-04-11 14:28:26 -0600

    Added find word count def

--- 102/Week12-utility.py
+++ 102/Week12-utility.py
@@ -5,3 +5,9 @@ def load_file(filename):
         return readfile
 def update_string(string1,string2,integer):
     return f'{string1[0:integer]}{string2}{string1[integer + 1:]}'
+def find_word_count(list,wordsearch):
+    numword = 0
+    for search in list:
+        search = search.split()
+        numword += search.count(wordsearch)
+    return numword
